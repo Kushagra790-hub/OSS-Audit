76a76cebdf09d324e49931795198f8928a468ea1
Kushagra790-hub <kushagraagarwal432@gmail.com>
2026-03-29 14:41:37 +0530

    script3.sh

--- script3.sh
+++ script3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ==========================================================
 # Script 3: Disk and Permission Auditor
-# Author: Utkarsh Sharma
+# Author: Kushagra Agarwal
 # Course: Open Source Software
 # Description: Checks directory size, ownership, and permissions
 # ==========================================================
